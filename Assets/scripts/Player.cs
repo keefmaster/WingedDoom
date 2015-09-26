02f461cd7aed9f42bc1e1c394a57a8eefaa7ed02
olehdd <olehdd@gmail.com>
2015-09-26 18:03:50 +0300

    working ai

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+//putin huilo
+	[System.Serializable]
+	public class PlayerStats {
+		public int Health = 100;
+		public int numberofLives = 3;
+	}
+
+	public PlayerStats playerStats = new PlayerStats();
+
+	public int fallBoundary = -20;
+
+	void Update () {
+		if (transform.position.y <= fallBoundary)
+			DamagePlayer (9999999);
+	}
+
+	public void DamagePlayer (int damage) {
+		playerStats.Health -= damage;
+		if (playerStats.Health <= 0) {
+			GameMaster.KillPlayer(this);
+		}
+	}
+
+}
