e762bda0d3b5a2dc56ba70738bf0705fa9bfeb26
leonid_iakubov <leonid_iakubov@epam.com>
2015-09-27 13:09:27 +0300

    added new enemy

---
+++ Assets/scripts/VatnikEnemy.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VatnikEnemy : MonoBehaviour {
+
+    [System.Serializable]
+    public class EnemyStats
+    {
+        public int maxHealth = 10;
+
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+        }
+
+        public int damage = 40;
+
+        public void Init()
+        {
+            curHealth = maxHealth;
+        }
+    }
+
+    public EnemyStats stats = new EnemyStats();
+
+    public Transform deathParticles;
+
+    public float shakeAmt = 0.1f;
+    public float shakeLength = 0.1f;
+
+    [Header("Optional: ")]
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+    void Start()
+    {
+        stats.Init();
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+        }
+
+        if (deathParticles == null)
+        {
+            Debug.LogError("No death particles referenced on Enemy");
+        }
+    }
+
+    public void DamageEnemy(int damage)
+    {
+
+        stats.curHealth -= damage;
+        if (stats.curHealth <= 0)
+        {
+            GameMaster.KillVatnikEnemy(this);
+        }
+
+        if (statusIndicator != null)
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D _colInfo)
+    {
+        Debug.Log("Collision detected " + _colInfo.gameObject.name);
+        Player _player = _colInfo.collider.GetComponent<Player>();
+        //Player _player = GameObject.FindGameObjectWithTag ("Player") as Player;
+        if (_player != null)
+        {
+            _player.DamagePlayer(stats.damage);
+            DamageEnemy(9999999);
+        }
+
+    }
+}
